74baf339517fc739a03d58605aa52cdd376ce5cf
perfjes <perniklaslongberg@gmail.com>
2018-04-12 16:26:49 +0200

    setup complete - working on fitting data to scikitlearn decision tree classifier

--- compute/modules/main.py
+++ compute/modules/main.py
@@ -1,9 +1,13 @@
 from compute.modules import dataSet
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 dataPath = r'../data/'
 
 df = dataSet.createDataFrame(dataPath, 'db.csv')
+
+print("* Person ID", '\n', df["id"].unique())
+
 dataSet.saveDataFrame(dataPath, df)
 
 df2 = dataSet.loadDataFrame(dataPath, 'df.csv')
-print(df2)
+#print(df2)
