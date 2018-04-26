e7c3e58b429a26869a9acc8fb2aee97d457d82be
perfjes <perniklaslongberg@gmail.com>
2018-04-26 13:11:42 +0200

    fixed some web interface issues, might need to look into frameworks for interactiveness

--- compute/webinterface/html.py
+++ compute/webinterface/html.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from compute.modules import dataset, regressor, classifier
 
 app = Flask(__name__, template_folder='templates')
@@ -7,23 +7,33 @@ file = ['df.csv']
 defaultdata = dataset.loaddataframe(file[0])
 
 
-@app.route('/')
-@app.route('/index')
+# Doesn't work with buttons...yet
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if request.form.get('loaddata') == 'loaddata':
+            print('load data')
+        elif request.form.get('savedata') == 'savedata':
+            print('save data')
+        elif request.form.get('regress') == 'regress':
+            print('regress')
+        elif request.form.get('classify') == 'classify':
+            print('classify')
+    elif request.method == 'GET':
+        return render_template('index.html', data=defaultdata.to_html())
 
     return render_template('index.html', data=defaultdata.to_html())
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 
 app.run()
